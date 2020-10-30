84b9c22c647c371afc2ceffdb9303a45a4c5457b
Roman Roibu <romanroibu@gmail.com>
2020-10-30 14:09:53 +0100

    Initialize AV packet with jpeg_buffer in JPEG_Writer

--- pupil_src/shared_modules/av_writer.py
+++ pupil_src/shared_modules/av_writer.py
@@ -302,9 +302,8 @@ class JPEG_Writer(AV_Writer):
 
     def encode_frame(self, input_frame, pts: int) -> T.Iterator[Packet]:
         # for JPEG we only get a single packet per frame
-        packet = Packet()
+        packet = Packet(input_frame.jpeg_buffer)
         packet.stream = self.video_stream
-        packet.update(input_frame.jpeg_buffer)
         packet.time_base = self.time_base
         packet.pts = pts
         # TODO: check if we still need dts here, as they were removed from MPEG_Writer
